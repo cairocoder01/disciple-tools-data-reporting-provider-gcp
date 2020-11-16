fc63fb8cdd1a55b4a02f7ed17b4c571579bd632e
zdmc23 <191707+zdmc23@users.noreply.github.com>
2020-11-15 22:04:34 -0500

    Initial Google Cloud Storage support

--- includes/admin/test/test_phpcbf.sh
+++ includes/admin/test/test_phpcbf.sh
@@ -5,7 +5,7 @@ set -e
 cd "$(dirname "${BASH_SOURCE[0]}")/../../../"
 
 if [ "$(php -r 'echo version_compare( phpversion(), "7.0", ">=" ) ? 1 : 0;')" != 1 ] ; then
-    vendor/bin/phpcbf disciple-tools-data-reporting-provider-sample.php
+    vendor/bin/phpcbf disciple-tools-data-reporting-provider-gcp.php
     exit
 fi
 
